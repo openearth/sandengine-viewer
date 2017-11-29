87ae164a77539a28d0d49c5798dabc0df85bcc93
CindyvdVries <cindyvdvries@gmail.com>
2017-11-29 13:50:42 +0100

    update v-meteo.js

--- src/components/v-meteo.js
+++ src/components/v-meteo.js
@@ -1,6 +1,10 @@
 import $ from 'jquery';
 import mapboxgl from 'mapbox-gl';
+import 'bokehjs'
 // import * as Bokeh from 'bokehjs'
+// require.import('bokehjs') as Bokeh
+
+// require('bokehjs')
 
 export default {
   name: 'VMeteo',
